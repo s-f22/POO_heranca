8840c8105bcce94dad04b95f5e587e04f2ddca04
Karamazov <samuel.silveira01@fatec.gov.sp.br>
2022-09-29 18:16:58 -0300

    heranca(apostila05): fim da listagem

--- ProfessorPesquisador.java
+++ ProfessorPesquisador.java
@@ -1,7 +1,9 @@
+//ProfessorPesquisador É-UM Professor
+//Professor É-UM Pessoa
 public class ProfessorPesquisador extends Professor {
 
   public void pesquisar() {
-    
+
     System.out.printf(
         "%s pesquisando...",
         nome);
